cd16f362cbf01d3d71e857d6fd26ce5087b95511
emilyclam <emilychilam@gmail.com>
2025-09-09 00:39:48 -0700

    changed to postgres db when running in production server

--- backend/recipebook/wsgi.py
+++ backend/recipebook/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipebook.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get("ENV") == "production":
+  django.setup()
+  call_command("migrate", interactive=False)
+  print("Migrations applied successfully.")
